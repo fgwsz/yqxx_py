b726342352d2b0210f8fcde75f0f2c99edbb50c8
fgwsz <3191000458@qq.com>
2024-05-29 16:02:36 +0800

    push text 增加创建è新è行操作

--- yqxx/push_info.py
+++ yqxx/push_info.py
@@ -38,8 +38,14 @@ def push_text_of_info(index_of_table=0):
     common.data_file_of_today_create()
     document=docx.Document(config.data_file_path_of_today())
     table=document.tables[index_of_table]
+    total_rows=len(table.rows) #获取表格的总行数
+    print("Total rows in the table:",total_rows)
     cell_col=1
     cell_row=common.count_non_empty_cells_in_column(table,cell_col)
+    if cell_row==total_rows:
+        table.add_row()
+        index_cell=table.cell(cell_row,0)
+        index_cell.text=f'{cell_row+1}.'
     cell=table.cell(cell_row,cell_col)
     clipboard_text=pyperclip.paste()
     cell.text=clipboard_text
